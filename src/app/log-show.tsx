d58a21f274f04b780705ad5db4392769c871a5bd
Matt Kollada <mattkollada@gmail.com>
2023-09-18 16:13:18 -0400

    changing log-show page styling to tailwind/native-wind

--- src/app/log-show.tsx
+++ src/app/log-show.tsx
@@ -1,29 +1,11 @@
-import { StyleSheet } from 'react-native';
-
 import { View } from '../components/Themed';
 import LogShowAccordion from '../components/LogShowAccordion';
 import React from 'react';
 
-export default function LogShowScreen() {
+export default function LogShowScreen() {  
   return (
-    <View style={styles.container} >
+    <View className='flex-1 justify-center'>
       <LogShowAccordion />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
