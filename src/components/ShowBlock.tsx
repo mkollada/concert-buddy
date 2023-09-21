79b271d498e848c4768a004c7f74da0fae833200
Matt Kollada <mattkollada@gmail.com>
2023-09-21 12:26:18 -0400

    styling tweaks to ShowBlock

--- src/components/ShowBlock.tsx
+++ src/components/ShowBlock.tsx
@@ -12,19 +12,20 @@ export const ShowBlock: React.FC<ShowProps> = ({
 }) => {
     
     return (
-        <View className='w-[90%] h-[10vh] flex-row justify-items-center place-items-center rounded-sm  p-5'>
+        // w-[90%] h-[10vh] justify-items-center place-items-center rounded-sm  p-5
+        <View className='flex-row h-[10vh] p-2'>
             
-            <View className="justify-center pr-5">
+            <View className="px-3 justify-center">
               <Text className="text-white font-bold">{show.date.substring(5,7)}.{show.date.substring(8,10)}</Text>
             </View>
-            <View className='flex-row px-3'>
+            <View className='flex-row justify-items-center px-2'>
                 <Image
-                className='w-[33%] aspect-square p-3'
+                className='aspect-square px-2'
                 source={{
                     uri: 'https://mcusercontent.com/f89c714d668e98a972a148b5b/images/d8ddef87-b281-b8c5-7b6f-9560c068ba2f.jpeg',
                 }}
                 />
-                <View className="px-3 justify-center">
+                <View className="flex-column justify-center px-2">
                   <Text className="text-white font-bold">{show.artistName}</Text>
                   <Text className="text-white">{show.venue}</Text>
                 </View>
