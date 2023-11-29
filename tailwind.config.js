21247a071b6c46b1de2246e28a334e2bcdfab3b7
Matt Kollada <mattkollada@gmail.com>
2023-11-29 12:18:25 -0500

    tailwind config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@
 module.exports = {
   content: [
     "./src/app/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}"
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
     colors: {
-      themeGray: '#1E2129'
+      themeGray: '#1E2129',
+      white: '#FFFFFF'
     }
   },
   plugins: [],
