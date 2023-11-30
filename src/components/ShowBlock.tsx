f05b5d5e90fdcffde5e4999adc58641b2acfd31d
Matt Kollada <mattkollada@gmail.com>
2023-11-30 13:09:25 -0500

    updating show block

--- src/components/ShowBlock.tsx
+++ src/components/ShowBlock.tsx
@@ -15,7 +15,7 @@ export const ShowBlock: React.FC<ShowProps> = ({
     return (
         <View className='flex-row h-[10vh] p-2'>
             
-            <View className="px-3 justify-center">
+            <View className="px-3 w-[10vh] justify-center">
               <Text className="text-white font-bold">{show.date.substring(5,7)}.{show.date.substring(8,10)}</Text>
             </View>
             <View className='flex-row justify-items-center px-2'>
