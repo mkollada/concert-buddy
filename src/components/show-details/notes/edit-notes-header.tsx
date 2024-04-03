26b59a717ff757c5a11eb1840e75ed97fb041bd2
Matt Kollada <mattkollada@gmail.com>
2024-04-03 13:01:02 -0400

    added show unsaved changes for notes

--- src/components/show-details/notes/edit-notes-header.tsx
+++ src/components/show-details/notes/edit-notes-header.tsx
@@ -3,30 +3,28 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
 interface EditNotesHeaderProps {
-    setModalVisible: (value: boolean) => void
+    handleCancel: () => void
+    handleSave: () => void
+    unsavedChanges: boolean
 }
 
 const EditNotesHeader: React.FC<EditNotesHeaderProps> = ({
-    setModalVisible
-}) => {  
-
-    const handleDone = () => {
-        setModalVisible(false)
-    }
+    handleCancel, handleSave, unsavedChanges }) => {  
 
     return (
             <View className="flex-row items-center justify-between p-2">
-                <TouchableOpacity onPress={handleDone} style={{ padding: 10 }}>
+                <TouchableOpacity onPress={handleCancel} style={{ padding: 10 }}>
                     <AntDesign name="arrowleft" color='white' size={24} />
                 </TouchableOpacity>
                 
                 <Text className="text-white font-bold text-lg">Notes</Text>
 
                 <TouchableOpacity 
-                    onPress={handleDone}
+                    onPress={handleSave}
                     style={{ padding: 10 }}
+                    activeOpacity={unsavedChanges ? 0.5 : 1}
                 >
-                    <Text className="text-white text-lg">Done</Text>
+                    <Text style={{ color: unsavedChanges ? '#9069F4' : '#A9A9A9'}}className="text-white text-lg">Save</Text>
                 </TouchableOpacity>
             </View>
     )
--- src/components/show-details/notes/edit-notes.tsx
+++ src/components/show-details/notes/edit-notes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, TextInput} from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import EditNotesHeader from "./edit-notes-header";
@@ -13,15 +13,36 @@ const EditNotes: React.FC<EditNotesProps> = ({
     notes, setNotes, setModalVisible
 }) => {
 
+    const [unsavedChanges, setUnsavedChanges] = useState(false)
+    const [tempNotes, setTempNotes] = useState(notes)
+
     const handleTextChange = (text: string) => {
-        setNotes(text)
+        setUnsavedChanges(true)
+        setTempNotes(text)
+    }
+
+    const handleSave = () => {
+        setNotes(tempNotes)
+        setUnsavedChanges(false)
+        setModalVisible(false)
+    }
+
+    const handleCancel = () => {
+        if(unsavedChanges){
+
+        } else {
+            setModalVisible(false)
+        }
     }
 
     return (
         
             <View className="flex-1 bg-black rounded-t2xl">
                 {/* <View className="flex-1 bg-black"> */}
-                <EditNotesHeader setModalVisible={setModalVisible}/>
+                <EditNotesHeader 
+                    handleCancel={handleCancel}
+                    handleSave={handleSave} 
+                    unsavedChanges={unsavedChanges}/>
                 <KeyboardAwareScrollView
                     enableOnAndroid={true}
                     extraHeight={300}
@@ -30,7 +51,7 @@ const EditNotes: React.FC<EditNotesProps> = ({
                     <View className='bg-themeGray flex-1 p-4'>
                         <TextInput 
                             className='flex-1 text-2xl text-white' 
-                            value={notes} 
+                            value={tempNotes} 
                             onChangeText={handleTextChange}
                             multiline
                             placeholder="Enter notes here..." />
