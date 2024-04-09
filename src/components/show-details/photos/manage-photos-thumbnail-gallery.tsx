0821c3e64bafbaf0e1ab0628aea63a3fc4f32d3a
Matt Kollada <mattkollada@gmail.com>
2024-04-09 11:18:53 -0400

    fixing manage photo plus box not being square

--- src/components/show-details/photos/manage-photos-thumbnail-gallery.tsx
+++ src/components/show-details/photos/manage-photos-thumbnail-gallery.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-import { Image, View, TouchableOpacity, Button, Text, Pressable } from 'react-native';
-import * as ImagePicker from 'expo-image-picker'
+import { Image, View, TouchableOpacity, Button, Text, Pressable, Dimensions } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+// Get the window width to calculate the size of the thumbnails
+const { width } = Dimensions.get('window');
+// Assuming there are 3 items per row, calculate the size minus some margin/padding
+const ITEM_SIZE = width / 3 - 8; // Adjust the 10 to your actual margin/padding
 
 interface ThumbnailGalleryProps {
     photoUrls: string[],
@@ -44,15 +49,15 @@ const ThumbnailGallery: React.FC<ThumbnailGalleryProps> = ({ photoUrls, setPhoto
     
 
     return (
-        <View className='flex-1'>
-            <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+        <View className='flex-1 items-center'>
+            <View className='flex-row flex-wrap p-2'>
                 {photoUrls.map((imgUri, idx) => (
-                    <Pressable key={idx} className="w-1/3 aspect-square p-1"  onPress={() => toggleSelectImage(idx)}>
-                        <Image source={{ uri: imgUri }} className={`w-full h-full rounded-lg ${selectedImages.includes(idx) ? 'opacity-50' : 'opacity-100'}`} />
+                    <Pressable className='p-1' key={idx} style={{ width: ITEM_SIZE, height: ITEM_SIZE, }} onPress={() => toggleSelectImage(idx)}>
+                        <Image className='w-[100%] h-[100%] rounded-lg' source={{ uri: imgUri }} style={{ opacity: selectedImages.includes(idx) ? 0.5 : 1 }} />
                     </Pressable>
                 ))}
-                <TouchableOpacity className="w-1/3 aspect-square p-1 items-center justify-center border-2 border-dashed border-gray-300" onPress={pickImage}>
-                    <Text className='text-white font-bold text-4xl'>+</Text>
+                <TouchableOpacity className='p-1 items-center justify-center border-2 border-dashed border-gray-300 rounded-lg' style={{ width: ITEM_SIZE, height: ITEM_SIZE }} onPress={pickImage}>
+                    <Text className='text-white font-bold text-2xl'>+</Text>
                 </TouchableOpacity>
             </View>
             <View>
@@ -67,3 +72,4 @@ const ThumbnailGallery: React.FC<ThumbnailGalleryProps> = ({ photoUrls, setPhoto
 }
 
 export default ThumbnailGallery;
+
