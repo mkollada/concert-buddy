b885bd10700948ccb607842c1d03a70f1f33e571
Matt Kollada <mattkollada@gmail.com>
2024-04-16 17:26:00 -0400

    adapting manage setlsit

--- src/components/show-details/setlist/manage-setlist.tsx
+++ src/components/show-details/setlist/manage-setlist.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { View, TextInput, Text, FlatList, StyleSheet, Keyboard, Platform, Alert } from "react-native";
+import { View, Text, TextInput, Button, Alert, TouchableOpacity, FlatList, KeyboardAvoidingView, Platform } from "react-native";
+import DraggableFlatList, { RenderItemParams } from "react-native-draggable-flatlist";
 import PageHeader from "../../utils/page-header";
+import { ScrollView } from "react-native-gesture-handler";
 
 interface ManageSetlistProps {
   setlist: string[];
@@ -13,89 +15,100 @@ const ManageSetlist: React.FC<ManageSetlistProps> = ({
   setSetlist,
   setModalVisible,
 }) => {
-    const [unsavedChanges, setUnsavedChanges] = useState(false)
-    const [songToAdd, setSongToAdd] = useState('');
+  const [unsavedChanges, setUnsavedChanges] = useState(false);
+  const [songToAdd, setSongToAdd] = useState("");
   const [tempSetlist, setTempSetlist] = useState<string[]>(setlist);
 
   const handleAddSong = () => {
-    console.log(songToAdd)
-    console.log('hey')
-    setTempSetlist([...tempSetlist, songToAdd])
-    setSongToAdd('')
-    setUnsavedChanges(true)
+    setTempSetlist((prevList) => [...prevList, songToAdd]);
+    setSongToAdd("");
+    setUnsavedChanges(true);
   };
 
-  const handleSave = () => {
-    setSetlist(tempSetlist)
-    setUnsavedChanges(false)
-    setModalVisible(false)
-}
+  const handleDelete = (index: number) => {
+    setTempSetlist((prevList) => prevList.filter((_, idx) => idx !== index));
+    setUnsavedChanges(true);
+  };
 
-  const renderItem = ({ item }: { item: string }) => (
-    <View className="bg-black p-1">
-        <View className="bg-themeGray p-2 w-full ">
-            <Text className="text-white">- {item}</Text>
-        </View>
-    </View>
-    
-  );
+  const handleDragEnd = ({ data }: { data: string[] }) => {
+    setTempSetlist(data);
+    setUnsavedChanges(true);
+  };
 
   const handleCancel = () => {
-    if(unsavedChanges){
-        Alert.alert(
-            "Unsaved Changes", // Title
-            "You have unsaved changes. Are you sure you want to go back without saving?", // Message
-            [
-                {
-                text: "Cancel",
-                onPress: () => console.log("Cancel Pressed"),
-                style: "cancel",
-                },
-                { text: "Confirm", style: "destructive", onPress: () => setModalVisible(false) },
-            ],
-            {
-                cancelable: true, // Whether to close the dialog on outside touch or not
-                onDismiss: () => console.log("Dialog dismissed"), // Callback when the alert is dismissed
-            }
-            );
+    if (unsavedChanges) {
+      Alert.alert(
+        "Unsaved Changes",
+        "You have unsaved changes. Are you sure you want to go back without saving?",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "Confirm", style: "destructive", onPress: () => setModalVisible(false) },
+        ],
+        {
+          cancelable: true,
+          onDismiss: () => console.log("Dialog dismissed"),
+        }
+      );
     } else {
-        setModalVisible(false)
+      setModalVisible(false);
     }
-}
+  };
+
+  const renderItem = ({ item, index }: {item: string, index: number}) => (
+    // <View className={`bg-black p-1 ${isActive ? 'bg-gray-500' : ''}`}>
+      <View className="flex-row items-center justify-between bg-themeGray p-2">
+        <Text className="text-white flex-1">- {item}</Text>
+        <TouchableOpacity onPress={() => handleDelete(tempSetlist.indexOf(item))} style={{ padding: 10 }}>
+          <Text style={{ color: 'red' }}>Delete</Text>
+        </TouchableOpacity>
+        {/* <TouchableOpacity onLongPress={drag} style={{ padding: 10 }}>
+          <Text style={{ color: 'blue' }}>Drag</Text>
+        </TouchableOpacity> */}
+      </View>
+    // </View>
+  );
 
   return (
     <View className='flex-1 bg-black'>
-        
-        <PageHeader
-            title="Setlist"
-            handleDonePress={handleSave}
-            doneText="Save"
-            handleCancelPress={handleCancel}
-            doneEnabled={unsavedChanges}
-        />
-        <View className="flex-1 items-center">
-            <View className='items-center w-1/2'>
-                <FlatList
-                    className="w-full"
-                    data={tempSetlist}
-                    renderItem={renderItem}
-                    keyExtractor={(item, index) => `song-${index}`}
-                    ListFooterComponent={
-                    <TextInput
-                        className="text-white p-5"
-                        value={songToAdd}
-                        onChangeText={text => setSongToAdd(text)}
-                        onSubmitEditing={() => handleAddSong()}
-                        placeholder="Add song..."
-                        placeholderTextColor='grey'
-                        returnKeyType="done"
-                        blurOnSubmit={false}
-                    />
-                    }
-                    // inverted // Invert the list to keep the input field at the bottom
-                />
-            </View>
-        </View>
+      <PageHeader
+        title="Setlist"
+        handleDonePress={() => {
+          setSetlist(tempSetlist);
+          setUnsavedChanges(false);
+          setModalVisible(false);
+        }}
+        doneText="Save"
+        handleCancelPress={handleCancel}
+        doneEnabled={unsavedChanges}
+      />
+      <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 0}
+      >
+        <FlatList
+        data={tempSetlist}
+        keyExtractor={(item, index) => `song-${index}`}
+        renderItem={renderItem}
+        ListFooterComponent={
+          <TextInput
+            className="text-white p-5"
+            value={songToAdd}
+            onChangeText={setSongToAdd}
+            onSubmitEditing={handleAddSong}
+            placeholder="Add song..."
+            placeholderTextColor="grey"
+            returnKeyType="done"
+            blurOnSubmit={false}
+          />
+        }
+      /> 
+      </KeyboardAvoidingView>
+      
     </View>
   );
 };
