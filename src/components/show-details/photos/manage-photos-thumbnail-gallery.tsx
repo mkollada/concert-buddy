a6cb0a0014555f363257438e112bbf50c9afbb0e
Matt Kollada <mattkollada@gmail.com>
2024-04-16 17:55:57 -0400

    fixed photos being in middle in thumbnail gallery

--- src/components/show-details/photos/manage-photos-thumbnail-gallery.tsx
+++ src/components/show-details/photos/manage-photos-thumbnail-gallery.tsx
@@ -51,24 +51,25 @@ const ThumbnailGallery: React.FC<ThumbnailGalleryProps> = ({ photoUrls, setPhoto
     
 
     return (
-        <View className='flex-1 items-center'>
-            <View className='flex-row flex-wrap p-2'>
-                {photoUrls.map((imgUri, idx) => (
-                    <Pressable className='p-1' key={idx} style={{ width: ITEM_SIZE, height: ITEM_SIZE, }} onPress={() => toggleSelectImage(idx)}>
-                        <Image className='w-[100%] h-[100%] rounded-lg' source={{ uri: imgUri }} style={{ opacity: selectedImages.includes(idx) ? 0.5 : 1 }} />
-                    </Pressable>
-                ))}
-                <TouchableOpacity className='p-1 items-center justify-center border-2 border-dashed border-gray-300 rounded-lg' style={{ width: ITEM_SIZE, height: ITEM_SIZE }} onPress={pickImage}>
-                    <Text className='text-white font-bold text-2xl'>+</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                {selectedImages.length > 0 && (
-                    <View className='p-5'>
-                        <Button color="red" title="Delete Selected" onPress={deleteSelectedImages} />
-                    </View>
-                )}
-            </View>
+        <View className='flex-1'>
+                <View className='flex-row flex-wrap p-2'>
+                    {photoUrls.map((imgUri, idx) => (
+                        <Pressable className='p-1' key={idx} style={{ width: ITEM_SIZE, height: ITEM_SIZE, }} onPress={() => toggleSelectImage(idx)}>
+                            <Image className='w-[100%] h-[100%] rounded-lg' source={{ uri: imgUri }} style={{ opacity: selectedImages.includes(idx) ? 0.5 : 1 }} />
+                        </Pressable>
+                    ))}
+                    <TouchableOpacity className='p-1 items-center justify-center border-2 border-dashed border-gray-300 rounded-lg' style={{ width: ITEM_SIZE, height: ITEM_SIZE }} onPress={pickImage}>
+                        <Text className='text-white font-bold text-2xl'>+</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    {selectedImages.length > 0 && (
+                        <View className='p-5'>
+                            <Button color="red" title="Delete Selected" onPress={deleteSelectedImages} />
+                        </View>
+                    )}
+                </View>
+            
         </View>   
     )
 }
