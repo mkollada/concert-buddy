ad4d4b8c5e761d2110f044f4b820cdbd8335941a
Matt Kollada <mattkollada@gmail.com>
2025-02-11 12:12:27 -0500

    updated code for vulnerabilities found

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -14,7 +14,7 @@ const ExpoSecureStoreAdapter = {
     },
   }
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL 
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
